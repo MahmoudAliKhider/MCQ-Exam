18991f84517f0f179b32508ab50f16d8548093fc
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-10-06 15:50:31 +0200

    register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -14,8 +14,48 @@ export class RegisterComponent implements OnInit {
   students:any[]= []
   constructor(private fb:FormBuilder ,private service:AuthService  , private router:Router , private toaster:ToastrService) { }
   ngOnInit(): void {
+  this.creatForm();
 
   }
 
- 
+ creatForm(){
+  this.userForm = this.fb.group({
+    username:['',Validators.required],
+    email:['',Validators.required,Validators.email],
+    password:['',Validators.required],
+    confirmpasswprd:['',Validators.required],
+  })
+ }
+
+ submit(){
+  const model ={
+    username:this.userForm.value.username,
+    email:this.userForm.value.email,
+    password:this.userForm.value.password,
+
+  }
+  let index = this.students.findIndex(item=> item.email == this.userForm.value.email)
+ if (index !== -1){
+  this.toaster.error("الايميل موجود مسبقا" , "" , {
+    disableTimeOut: false,
+    titleClass: "toastr_title",
+    messageClass: "toastr_message",
+    timeOut:5000,
+    closeButton: true,
+  })
+ }else{
+  this.service.createUser(model).subscribe((res:any) => {
+    this.toaster.success("تم انشاء الحساب بنجاح" , "" , {
+      disableTimeOut: false,
+      titleClass: "toastr_title",
+      messageClass: "toastr_message",
+      timeOut:5000,
+      closeButton: true,
+    })
+
+    this.router.navigate(['/subjects'])
+ })
+
+}
+}
 }
