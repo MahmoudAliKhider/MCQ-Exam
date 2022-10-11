f85eb07e42a4a73ee08b1f3c8b3ad4ad6d905868
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-10-11 16:57:13 +0200

    List All Students With Degrees

--- src/app/doctor/components/students/students.component.ts
+++ src/app/doctor/components/students/students.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
+
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
@@ -16,9 +17,46 @@ export class StudentsComponent implements OnInit {
 
 
   ngOnInit(): void {
-
+  this.getStudents()
   }
 
+getStudents(){
+  this.service.getUsers('students').subscribe((res:any)=>{
 
+   this.dataSource= res?.map((student:any)=>{
+    //if عشان لو الطالب لسه ممتحنش
+    if(student?.subjects){
+      return student?.subjects?.map((sub:any)=>{
+        return {
+          name:student.username,
+          subjectName:sub.name,
+          degree:sub.degree
+        }
+       })
+    }else {
+      return [{
+        name:student.username,
+        subjectName:"-",
+        degree:"-"
+      }]
+    }
+
+   })
+   this.datatable=[];
+   this.dataSource.forEach((item:any) => {
+    item.forEach((subIt:any)=>{
+      this.datatable.push({
+        name:subIt.name,
+        subjectName:subIt.subjectName,
+        degree:subIt.degree
+      }
+
+      )
+    })
+
+   });
+   console.log(this.datatable)
+  })
+}
 
 }
